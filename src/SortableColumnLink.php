4b2c793d0e167b0508b29a4896de5ba5f18e1ae0
Colin Hall <colin@macsi.co.uk>
2019-06-24 20:35:00 +0000

    Apply fixes from StyleCI

--- src/SortableColumnLink.php
+++ src/SortableColumnLink.php
@@ -4,7 +4,6 @@ namespace MacsiDigital\Sortable;
 
 class SortableColumnLink
 {
-
     /**
      * @param array $parameters
      *
@@ -14,7 +13,7 @@ class SortableColumnLink
     {
 
         // $column, $variables
-        list($column, $variables) = self::parseParameters($parameters);
+        [$column, $variables] = self::parseParameters($parameters);
 
         $label = self::formLabel($column, $variables);
 
@@ -25,7 +24,6 @@ class SortableColumnLink
         return '<a href="'.$href.'" '.$vars.'>'.$label.'</a>';
     }
 
-
     /**
      * @param array $parameters
      *
@@ -33,15 +31,15 @@ class SortableColumnLink
      */
     public static function parseParameters(array $parameters)
     {
-        $column    = $parameters[0];
-        if(isset($parameters[1])){
+        $column = $parameters[0];
+        if (isset($parameters[1])) {
             $variables = $parameters[1];
         } else {
             $variables = [];
         }
+
         return [$column, $variables];
     }
-
 
     /**
      * @param string $column
@@ -51,26 +49,26 @@ class SortableColumnLink
      */
     private static function formLabel($column, $variables)
     {
-        if(isset($variables['label'])){
+        if (isset($variables['label'])) {
             $label = $variables['label'];
         } else {
             $label = ucfirst($column);
         }
-        if(config('sortable.use-icon')){
+        if (config('sortable.use-icon')) {
             $sorts = [];
-            foreach(request()->query() as $k => $v){
-                if($k == 'sort'){
+            foreach (request()->query() as $k => $v) {
+                if ($k == 'sort') {
                     $sorts = explode(',', $v);
                 }
             }
-            if($sorts != []){
+            if ($sorts != []) {
                 $column_in_sort = false;
-                foreach($sorts as $sort){
-                    list($field, $direction) = explode('-', $sort);
-                    if($field == $column){
-                        if($direction == 'asc'){
+                foreach ($sorts as $sort) {
+                    [$field, $direction] = explode('-', $sort);
+                    if ($field == $column) {
+                        if ($direction == 'asc') {
                             $label .= ' '.config('sortable.asc-icon');
-                        } else if ($direction == 'desc') {
+                        } elseif ($direction == 'desc') {
                             $label .= ' '.config('sortable.desc-icon');
                         }
                     }
@@ -79,6 +77,7 @@ class SortableColumnLink
                 $label .= ' '.config('sortable.none-icon');
             }
         }
+
         return $label;
     }
 
@@ -87,40 +86,39 @@ class SortableColumnLink
         $href = '/'.request()->path().'?';
         $query = '';
         $sorts = [];
-        foreach(request()->query() as $k => $v){
-            if($k != 'sort'){
+        foreach (request()->query() as $k => $v) {
+            if ($k != 'sort') {
                 $href .= $k.'='.$v;
             } else {
                 $sorts = explode(',', $v);
             }
         }
-        if($sorts != []){
+        if ($sorts != []) {
             $i = 1;
             $column_in_sort = false;
-            foreach($sorts as $sort){
-                list($field, $direction) = explode('-', $sort);
-                if($field == $column){
+            foreach ($sorts as $sort) {
+                [$field, $direction] = explode('-', $sort);
+                if ($field == $column) {
                     $column_in_sort = true;
                     $query .= self::formThisColumn($direction, $column, $i);
                 } else {
-                    if($i > 1){
+                    if ($i > 1) {
                         $query .= ',';
                     }
                     $query .= $field.'-'.$direction;
                     $i++;
                 }
-                
             }
-            if(!$column_in_sort){
-                if($i>1){
-                    $query .= ',';    
+            if (! $column_in_sort) {
+                if ($i > 1) {
+                    $query .= ',';
                 }
                 $query .= $column.'-asc';
             }
         } else {
             $query .= $column.'-asc';
         }
-        if($query != ''){
+        if ($query != '') {
             return $href.'&sort='.$query;
         } else {
             return $href;
@@ -129,24 +127,26 @@ class SortableColumnLink
 
     private static function formThisColumn($direction, $column, &$i)
     {
-        if($direction == 'desc'){
+        if ($direction == 'desc') {
             return '';
-        } else if($direction == 'asc'){
+        } elseif ($direction == 'asc') {
             $pre = '';
-            if($i > 1){
+            if ($i > 1) {
                 $pre = ',';
             }
             $i++;
+
             return $pre.$column.'-desc';
         }
     }
 
     private static function formParameters($variables)
     {
-        $string = "";
-        foreach($variables as $k => $v){
+        $string = '';
+        foreach ($variables as $k => $v) {
             $string .= ' '.$k.'="'.$v.'"';
         }
+
         return $string;
     }
 }
